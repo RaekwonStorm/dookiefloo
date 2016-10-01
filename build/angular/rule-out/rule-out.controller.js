b9f4043e49c3fc4bdcc6a58be90dd8a37b6bd510
Reed Branson <reed.branson@gmail.com>
2016-10-01 14:09:34 -0400

    ruleouts yo

--- build/angular/rule-out/rule-out.controller.js
+++ build/angular/rule-out/rule-out.controller.js
@@ -1,1 +1,20 @@
-app.controller('ruleoutCtrl', function () {});
+app.controller('ruleoutCtrl', function($scope, decisionTrees) {
+
+  $scope.ruleOuts = decisionTrees.ruleOuts;
+  $scope.showResult = false;
+  $scope.result;
+
+  $scope.clickHandler = (gender, obese, pregnant) => {
+    if (gender === "male") {
+      $scope.result = $scope.diagnoses['rlqp'][gender]['obese'][obese]
+      console.log($scope.result);
+    } else {
+      $scope.result = $scope.diagnoses['rlqp'][gender][obese][pregnant];
+    }
+    $scope.showResult = true;
+    console.log(gender, obese, pregnant);
+  }
+
+  console.log("Did it blend? " ,decisionTrees);
+
+});
