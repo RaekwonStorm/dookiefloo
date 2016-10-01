f244eb026d1429dc5982611636edb010bbe820f5
Reed Branson <reed.branson@gmail.com>
2016-10-01 13:58:16 -0400

    OBESE MALE NOT PREGNERT

--- build/angular/diagnosis/diagnosis.controller.js
+++ build/angular/diagnosis/diagnosis.controller.js
@@ -1,4 +1,18 @@
 app.controller('diagnosisCtrl', function ($scope, decisionTrees) {
+  $scope.diagnoses = decisionTrees.diagnoses;
+  $scope.showResult = false;
+  $scope.result;
+
+  $scope.clickHandler = (gender, obese, pregnant) => {
+    if (gender === "male") {
+      $scope.result = $scope.diagnoses['rlqp'][gender]['obese'][obese]
+      console.log($scope.result);
+    } else {
+      $scope.result = $scope.diagnoses['rlqp'][gender][obese][pregnant];
+    }
+    $scope.showResult = true;
+    console.log(gender, obese, pregnant);
+  }
 
 	console.log("Did it blend? " ,decisionTrees);
 });
